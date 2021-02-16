452ceac75eab1cd7a3252bb3260d34578af522e4
Pavel Zhukov <zhukovpavel58@mail.ru>
2021-02-17 01:07:16 +0300

    feat: Todo list | Script\script.js

---
+++ script/script.js
@@ -0,0 +1,98 @@
+'use strict';
+
+
+const todoControl = document.querySelector('.todo-control'), 
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed');
+
+
+const todoData = [];  
+
+const render = function() {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+    
+    todoData.forEach(function(item){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' + 
+            '<div class="todo-buttons">' + 
+                '<button class="todo-remove"></button>' + 
+                '<button class="todo-complete"></button>' + 
+            '</div>';
+
+        if(item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+        
+        headerInput.value = '';
+        
+        localStorage.setItem('localTodoData', JSON.stringify(todoData));
+
+        const btnTodoComplete = li.querySelector('.todo-complete');
+
+        btnTodoComplete.addEventListener('click', function(){
+            item.completed = !item.completed;
+            render();
+        });
+        const btnRemove = li.querySelector('.todo-remove');
+
+        btnRemove.addEventListener('click', function() {
+            todoData.splice(item, 1);
+            localStorage.removeItem('localTodoData');
+            render();
+
+        });
+        
+    });
+};
+
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+    if (headerInput.value !== '') {
+    let newTodo = {
+        value: headerInput.value,
+        completed: false
+
+    };
+
+    todoData.push(newTodo);
+
+    render();
+    }
+});
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    if (event.keyCode === 13 || headerInput.value !== '') {
+
+    let newTodo = {
+        value: headerInput.value,
+        completed: false
+
+    };
+
+    todoData.push(newTodo);
+    
+    render();
+}
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let load = JSON.parse(localStorage.getItem("localTodoData"));
+    if (load !== null) {
+      load.forEach(function(item) {
+        todoData.push(item);
+    });
+    }
+
+    render();
+});
+
+render();
